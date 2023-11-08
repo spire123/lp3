41e38322d37480a764d2774235d6184ea2184c2c
spire123 <134633985+spire123@users.noreply.github.com>
2023-11-09 01:41:06 +0530

    Update Bank.sol

--- Bank.sol
+++ Bank.sol
@@ -1,48 +1,25 @@
 //BCT Practical 3: Smart contract for Bank 
-pragma solidity ^0.4.13;
+pragma solidity ^0.8.0;
 
-contract SimpleBank{
+contract Bank {
+    int private balance; 
 
-    mapping (address => uint) private balances;
-
-    address public owner;
-
-    event LogDepositMade(address accountAddress, uint amount);
-
-    function Bank() public{
-        /* set the owner to the creator of this contract */
-        owner = msg.sender;
+    constructor() {
+        balance = 0;  
     }
 
-    function enroll() public returns (uint){
-        address user = msg.sender;
-
-        balances[user] = 1000;
-        return  user.balance;
+    function getBalance() public view returns (int) {
+        return balance;
     }
 
-    function deposit() public payable returns(uint){
-        address user = msg.sender;
-
-        balances[user] += msg.value;
-        emit LogDepositMade(user, msg.value);
-        return balances[user];
+    function deposit(int amount) public {
+        require(amount > 0, "Amount must be greater than 0");
+        balance += amount;
     }
 
-    function withdraw(uint withdrawAmount) public returns(uint remainingBal){
-        address user = msg.sender;
-        require(balances[user] >= withdrawAmount);
-        balances[user] -= withdrawAmount;
-        user.transfer(withdrawAmount);
-        return balances[user];
-    }
-
-    function balance() public view returns(uint){
-        address user = msg.sender;
-        return balances[user];
-    }
-
-    function () public {
-        revert();
+    function withdraw(int amount) public {
+        require(amount > 0, "Amount must be greater than 0");
+        require(amount <= balance, "Insufficient balance");
+        balance -= amount;
     }
 }
