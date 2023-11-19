aab9d8d9d5f72f4dd4f0667119dc299093a954c1
spire123 <134633985+spire123@users.noreply.github.com>
2023-11-20 00:04:21 +0530

    Update NQueen.java

--- NQueen.java
+++ NQueen.java
@@ -1,24 +1,16 @@
-//DAA Practical no 5: Solve n-queen problem using backtracking
+//DAA Practical 4: N-Queen using Backtracking
+
+import java.util.Scanner;
 
 public class NQueen {
-
-	public static void main(String[] args) {
-		int n=8;
-		int board[][] = new int[n][n];
-		
-		System.out.println("The solution for " + n + " Queen is: ");
-		solve(0,n,board);
-
-	}
-
-	static void solve(int col, int n, int board[][]) {
-		//if all queen is placed
+	
+	static void solve(int col, int n, int board[][])
+	{
 		if(col==n)
 		{
-			//for printing the queens
-			for(int i=0; i<board.length; i++) {
-				for(int j=0; j<board.length; j++) {
-					System.out.print(" "+board[i][j]);
+			for(int i=0; i<n; i++) {
+				for(int j=0; j<n; j++) {
+					System.out.print(" " + board[i][j]);
 				}
 				System.out.println();
 			}
@@ -27,48 +19,52 @@ public class NQueen {
 		}
 		
 		for(int row=0; row<n; row++) {
-			if(issafe(row,col,n,board)) 
+			if(issafe(col, row, n, board))
 			{
-				board[row][col] = 1;
-				//if one queen is place then 
-				//increment column and call solve method again
-				solve(col+1,n, board);
-				//same row next col = 0
+				board[row][col]=1;
+				solve(col+1, n, board);
 				board[row][col] = 0;
 			}
 		}
 	}
 	
-	static boolean issafe(int row, int col, int n, int board[][]) 
+	static boolean issafe(int col, int row, int n, int board[][])
 	{
-		int x = row;
-		int y = col;
-		
+		int x= row;
+		int y= col;
 		while(y>=0)
 		{
-			if(board[x][y]==1)
-				return false;
+			if(board[x][y]==1) return false;
 			y--;
 		}
 		y=col;
-		//checking upper diagonal
 		while(x>=0 && y>=0)
 		{
-			if(board[x][y]==1)
-				return false;
-			x--; 
+			if(board[x][y]==1) return false;
 			y--;
+			x--;
 		}
-		x=row; 
+		x=row;
 		y=col;
-		while(x<n && y>=0)
+		while(x<n && y>=0) 
 		{
-			if(board[x][y] == 1)
-				return false;
-			x++;
+			if(board[x][y]==1) return false;
 			y--;
+			x++;
 		}
 		return true;
 	}
+	
+	public static void main(String[] args) {
+		System.out.println("Enter the number for n queen: ");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int board[][] = new int[n][n];
+		
+		System.out.println("Solution for " + n + " queens is: ");
+		solve(0, n, board);
+
+	}
 
 }
+
